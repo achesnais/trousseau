8163c0b2792cfe2a92004d65ca83c680f9f445dc
Antoine Chesnais <achesnais@gmail.com>
2017-09-24 12:10:17 +0200

    Add proportions, rsort, rsort-by and time ns

--- src/achesnais/trousseau.clj
+++ src/achesnais/trousseau.clj
@@ -5,3 +5,16 @@
 
 (defn map-keys [f m]
   (reduce-kv (fn [m k v] (assoc m (f k) v)) {} m))
+
+(defn proportions [coll]
+  (let [n (count coll)]
+    (map-vals #(float (/ % n)) (frequencies coll))))
+
+(defn- reverse-compare [x y]
+  (compare y x))
+
+(defn rsort [coll]
+  (sort reverse-compare coll))
+
+(def rsort-by [keyfn coll]
+  (sort-by keyfn reverse-compare coll))
---
+++ src/achesnais/trousseau/time.clj
@@ -0,0 +1,23 @@
+(ns achesnais.trousseau.time)
+
+(defprotocol ITimeCoercions
+  (utc-zoned-date-time [x])
+  (sql-timestamp [x])
+  (inst [x]))
+
+(extend-protocol ITimeCoercions
+
+  java.util.Date
+  (utc-zoned-date-time [x] (java.time.ZonedDateTime/ofInstant (.toInstant x) (java.time.ZoneId/of "UTC")))
+  (sql-timestamp [x] (java.sql.Timestamp. (.getTime x)))
+  (inst [x] x)
+
+  java.time.ZonedDateTime
+  (zoned-date-time [x] x)
+  (sql-timestamp [x] (java.sql.Timestamp. (.toEpochMilli (.toInstant x))))
+  (inst [x] (java.util.Date/from (.toInstant x)))
+
+  java.sql.Timestamp
+  (utc-zoned-date-time [x] (java.time.ZonedDateTime/ofInstant (.toInstant x) (java.time.ZoneId/of "UTC")))
+  (sql-timestamp [x] x)
+  (inst [x] (java.util.Date. (.getTime x))))
